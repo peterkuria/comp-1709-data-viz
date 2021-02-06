96aceacc2c42da81f028077d0e0165f05d559610
peterkuria <peter.kuria.cloud@gmail.com>
2021-02-06 15:14:44 +0300

    [feat]: edit scatter plot in matplotlib

--- L02 proportion/scatter_plot.py
+++ L02 proportion/scatter_plot.py
@@ -2,6 +2,8 @@
 Scatter plots is usu to compare two variables, 
 or three if you are plotting in 3 dimensions, looking for 
 correlation or groups.
+The plt.scatter allows us to not only plot on x and y, but it also lets us decide on the color, size, and type of marker we use. There are a bunch of marker options, see the Matplotlib Marker Documentation for all of your choices.
+
 """
 
 import matplotlib.pyplot as plt
@@ -9,7 +11,10 @@ import matplotlib.pyplot as plt
 x = [1,2,3,4,5,6,7,8]
 y = [5,2,4,2,1,4,5,2]
 
-plt.scatter(x,y, label='skitscat', color='k', s=25, marker="o")
+# (See matplotlib maker types](https://matplotlib.org/3.3.3/api/markers_api.html)
+plt.scatter(x,y, label='skitscat', color='k', s=25, marker="o", s=10)
+
+plt.scatter(x,y, label='skitscat', color='k', s=25, marker="*")
 
 plt.xlabel('x')
 plt.ylabel('y')
