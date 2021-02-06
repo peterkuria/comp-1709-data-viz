82449612750277092e5a0e98c16b4a0aa30271b9
peterkuria <peter.kuria.cloud@gmail.com>
2021-02-06 13:19:19 +0300

    [feat]: create virtual env for the lab with pipenv

--- L02 proportion/bar_charts_histograms.py
+++ L02 proportion/bar_charts_histograms.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-# plotting a single line bar	  
+
+# plotting a single line bar
 plt.bar([1,3,5,7,9],[5,2,7,8,2], label="Example one")
 
 plt.bar([2,4,6,8,10],[8,6,2,5,6], label="Example two", color='g')
